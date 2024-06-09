10585c6b5f704c7614c7453549f6b4ec1b81ebaf
40in <sorokin@VM2.myguest.virtualbox.org>
2024-06-09 19:17:08 +0300

    lab9 is ready

--- backend/src/main/java/ru/iu3/backend/controllers/LoginController.java
+++ backend/src/main/java/ru/iu3/backend/controllers/LoginController.java
@@ -14,6 +14,7 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/auth")
 public class LoginController {
@@ -58,5 +59,6 @@ public class LoginController {
         }
         return new ResponseEntity(HttpStatus.UNAUTHORIZED);
     }
+
 
 }
